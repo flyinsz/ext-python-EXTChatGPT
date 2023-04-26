f1a4012a4ff7237c27989970aa447e4eb11e56b3
flyinsz <donnychow@qq.com>
2023-04-26 13:40:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # 聊天机器人生效的微信群名词
-chatroom_name_list = ['extpy框架测试']
+chatroom_name_list = ['']
 # 插件启动时机器人向文件助手发送的提示信息
 plugin_info = '【ChatGPT聊天机器人】插件已启动\n本插件仅在以下版本测试运行:\nEXT框架版本:3.7.5.2\n微信PC版本:3.7.5.23'
 # 机器人在EXT框架里的pid
-pid = 0
+pid = 20356
 # openai申请的api_key
-api_key = ''
+api_key = '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'
